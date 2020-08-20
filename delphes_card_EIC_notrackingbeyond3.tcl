d9dd9d29b6a8a7853fbaee41a6fd2fb1369ae055
marratia <marratia@cern.ch>
2020-08-20 11:50:23 -0700

    adding HCAL parametrization (3.0-4.0) to customization.tcl

--- delphes_card_EIC_notrackingbeyond3.tcl
+++ delphes_card_EIC_notrackingbeyond3.tcl
@@ -415,8 +415,8 @@ module SimpleCalorimeter HCal {
   set ResolutionFormula {    (eta <= -1.0 && eta>-4.0)                       * sqrt(energy^2*0.10^2 + energy*0.50^2)+
                              (eta <= 1.0 && eta>-1.0 )                       * sqrt(energy^2*0.10^2 + energy*1.00^2)+
                               (eta <= 3.0  && eta>1.0 )                       * sqrt(energy^2*0.10^2 + energy*0.50^2)+
-                              (eta <= 4.0  && eta>3.0 )                       * sqrt(energy^2*0.05^2 + energy*0.45^2)}
-
+      (eta <= 4.0  && eta>3.0 ) * sqrt(energy^2*[expr{$HCAL_CONSTANT*$HCAL_CONSTANT}] + energy*[expr{$HCAL_STOCHASTIC*$HCAL_STOCHASTIC}])}
+    
 }
 
 
